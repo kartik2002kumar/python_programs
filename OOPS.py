a2168105fcd4c9454dc736cc21c881b10a1101ca
Kartik Kumar <kartikthakurela73@gmail.com>
2026-02-10 12:52:46 +0530

    oop new

---
+++ OOPS.py
@@ -0,0 +1,39 @@
+# class demo:
+#     pass
+# ob1=demo()
+
+# print(type(ob1))
+
+
+
+
+# class car:
+#     cname='omni'
+#     ccolour='white'
+# tushaar=car()
+# print(tushaar.ccolour)
+
+
+
+
+
+# class zoo:
+#     a='anaconda'
+#     b='bear'
+#     c='cat(big)'
+# tushar=zoo()
+# udit=zoo()
+
+# print(zoo.a,zoo.b,zoo.c)
+# print(tushar.a,tushar.b,tushar.c)
+# print(udit.a,udit.b,udit.c)
+
+# zoo.c='crocodile'
+# print(zoo.a,zoo.b,zoo.c)
+# print(tushar.a,tushar.b,tushar.c)
+# tushar.c='fox'
+# print(tushar.a,tushar.b,tushar.c)
+
+
+
+
---
+++ oopPROP.py
@@ -0,0 +1,60 @@
+# class bank:
+#     bname='SBI'
+#     bloc='delhi'        #Static member
+#     bIFSC='SBI0324564'
+
+# sahil=bank()
+# sanyam=bank()
+
+
+
+# sahil.name='sahil'
+# sahil.phno=98745621
+# sahil.addr='noida sect2'
+# sahil.email='sahil4545@gmail.com'
+# sahil.accno=59789466
+
+
+# sanyam.name='sahil'
+# sanyam.phno=98745621
+# sanyam.addr='noida sect2'
+# sanyam.email='sahil4545@gmail.com'
+# sanyam.accno=59789466
+
+
+
+
+# WAp to create a class hospital with minumuu  the statiuc number ,two objects,with 5 object member
+
+
+class hospital:
+    hname='sharda hospital'
+    hloc='greater noida'
+    htype='private'
+
+ramu=hospital()
+shyam=hospital()
+
+
+ramu.ptname='ramu'
+ramu.disease='snake bite'
+ramu.wardno= 5
+ramu.cndoctor='Dr. SP'
+ramu.Bgroup= 'B+'
+
+
+shyam.ptname='shyam'
+shyam.disease='typhoid'
+shyam.wardno= 8
+shyam.cndoctor='Dr. KD'
+shyam.Bgroup= 'A+'
+
+
+print(hospital.hname)
+
+
+
+
+
+
+
