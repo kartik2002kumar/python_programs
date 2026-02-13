8631fe152aef6169e7b6378f4e2b2031b4185317
Kartik Kumar <kartikthakurela73@gmail.com>
2026-02-13 22:09:49 +0530

    instance modify

---
+++ instance change consteructor.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def change_name(self, new_name):
+        self.name = new_name
+s1 = Student("Kartik")
+print(s1.name)
+
+s1.change_name("Rahul")
+print(s1.name)
